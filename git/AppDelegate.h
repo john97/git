904ce4d92a38bcafc766ff6b27a4329798bfb53d
john97 <keshavgoyal9697@gmail.com>
2017-05-24 15:14:47 +0530

    Initial Commit

---
+++ git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  git
+//
+//  Created by Keshav Goyal on 24/05/17.
+//  Copyright © 2017 Keshav Goyal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git
+//
+//  Created by Keshav Goyal on 24/05/17.
+//  Copyright © 2017 Keshav Goyal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
